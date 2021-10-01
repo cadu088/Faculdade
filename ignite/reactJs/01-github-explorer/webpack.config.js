322bae85c1442f2ef228f276872c565daaf81ae0
Carlos Rodrigues <carloseduardors088@gmail.com>
2021-10-01 10:53:09 -0300

    webpack

--- ignite/reactJs/01-github-explorer/webpack.config.js
+++ ignite/reactJs/01-github-explorer/webpack.config.js
@@ -1,21 +1,21 @@
 const path = require('path')
 
-module.exports ={
-	entry: path.resolve(__dirname, 'src', 'index.jsx'),
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
 
-	output:{
-		path: resolve(__dirname, 'dist'), 
-		filename: 'bundle.js',
-	},
-resolve: {
-	extensions: ['.js', '.jsx'],
-}, 
-module: {
-	rules:[{
-		test: /\.js$/,
-		exclude: /node_modules/,
-		use: 'babel-loader',
-	}]
-}
+    output: {
+        path: resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [{
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            use: 'babel-loader',
+        }],
+    }
 
 }
