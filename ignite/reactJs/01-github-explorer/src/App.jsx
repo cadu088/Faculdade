37333a8c5e92e582203a2172d1c58605dc883355
Carlos Eduardo Rodrigues da Silva <carlos.rodrigues@zema.com>
2021-10-01 17:49:06 -0300

    finalizando config

--- ignite/reactJs/01-github-explorer/src/App.jsx
+++ ignite/reactJs/01-github-explorer/src/App.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import './styles/global.scss';
+
 export function App(){
 	return <h1> Hello World </h1>
 }
--- ignite/reactJs/01-github-explorer/webpack.config.js
+++ ignite/reactJs/01-github-explorer/webpack.config.js
@@ -1,21 +1,42 @@
+const { resolve } = require('path')
 const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+const IsDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
-
-    output: {
-        path: resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    module: {
-        rules: [{
-            test: /\.jsx$/,
-            exclude: /node_modules/,
-            use: 'babel-loader',
-        }],
-    }
-
+	mode: IsDevelopment? 'development' : 'production',
+	devtool: IsDevelopment? 'eval-source-map' : 'source-map',
+	entry: path.resolve(__dirname, 'src', 'index.jsx'),
+	output: {
+		path: path.resolve(__dirname, 'dist',),
+		filename: 'bundle.js'
+	},
+	resolve: {
+    extensions: ['.js', '.jsx']
+  },
+	devServer: {
+    //contentBase: path.resolve(__dirname, 'public'), 
+    static: {
+      directory: path.resolve(__dirname, 'public')}
+  },
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: path.resolve(__dirname, 'public', 'index.html'),
+		})
+	],
+	module: {
+  rules: [
+		{
+			test: /\.jsx$/,
+			exclude: /node_modules/,
+			use: 'babel-loader',
+		}, 
+		{
+			test: /\.scss$/,
+			exclude: /node_modules/,
+			use: ['style-loader', 'css-loader', 'sass-loader'],
+		}, 
+	]
+	}
 }
