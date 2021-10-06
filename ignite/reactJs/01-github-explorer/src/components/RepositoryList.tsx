787e038de2f0ae7847051061676c5df3e8ab7839
Carlos Eduardo Rodrigues da Silva <carlos.rodrigues@zema.com>
2021-10-06 16:37:05 -0300

    finalizando passos

--- ignite/reactJs/01-github-explorer/src/components/RepositoryList.tsx
+++ ignite/reactJs/01-github-explorer/src/components/RepositoryList.tsx
@@ -2,10 +2,15 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repository.scss'
 import { useEffect, useState } from "react";
 
+interface Repository{
+	name: string;
+	description: string;
+	html_url: string;
+}
 
 export function RepositoryList(){
 
-	const [Repositories, setRepositories] = useState([]);
+	const [Repositories, setRepositories] = useState<Repository[]>([]);
 	useEffect(() => {
    fetch('https://api.github.com/users/cadu088/repos')
 	 .then(response => response.json())
@@ -13,7 +18,7 @@ export function RepositoryList(){
 	}, [] )
 
     return(
-        <setion className="repository-list">
+        <section className="repository-list">
             <h1>Lista de repositorios</h1>
             <ul>
 							{Repositories.map( repository=> {
@@ -22,6 +27,6 @@ export function RepositoryList(){
 
             </ul>
 
-        </setion>
+        </section>
     );
 }
