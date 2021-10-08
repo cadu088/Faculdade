154a15bde7dbb16465598049aac8e4281e00caa7
Carlos Eduardo Rodrigues da Silva <carlos.rodrigues@zema.com>
2021-10-08 16:53:24 -0300

    chapter 2

---
+++ ignite/reactJs/02-dtmoney/src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header";
+
+export function App() {
+  return (
+    <div className="App">
+     <Header/>
+		 <GlobalStyle/>
+    </div>
+  );
+}
+
