fd5e8207102798f530fb4eade22af0f099984590
Carlos Rodrigues <carloseduardors088@gmail.com>
2021-10-26 10:13:23 -0300

    TDE Java

--- TDE C# for Java/exercicios.java
+++ TDE C# for Java/exercicios.java
@@ -27,73 +27,73 @@ public class exercicios
 
 
 
-    //exercicio 03
-    Scanner ler = new Scanner(System.in);
-    Double valores; 
-    System.out.println("Digite 10 valores para somar:");
-    valores = (double) 0;
+    // // //exercicio 03
+    // // Scanner ler = new Scanner(System.in);
+    // // Double valores; 
+    // // System.out.println("Digite 10 valores para somar:");
+    // // valores = (double) 0;
 
 
-    for (int i = 0; i < 10; i++){
-      System.out.print(i + 1 +"º: ");
-      valores += ler.nextDouble();
-    }
-    System.out.print("A soma dos valores é: " + valores);
-
-
-
-
-    // exercicio 02
-    Scanner ler = new Scanner(System.in);
-    Double codigo, qtrGramas, resultado;
-    String minerio;
-    System.out.print("Digite o código do minério: ");
-    codigo = ler.nextDouble();
-    System.out.print("Digite a quantidade em gramas: ");
-    qtrGramas = ler.nextDouble();
-
-    if (codigo <= 4){
-      if (codigo == 1){
-        minerio = "Fosfato";
-        resultado = (qtrGramas * 100) / 2;
-      }
-      else if (codigo == 2){
-        minerio = "Nióbio";
-        resultado = (qtrGramas + 100) / 3;
-      }
-      else if (codigo == 3){
-        minerio = "Titânio";
-        resultado = (qtrGramas * 1.5) / 5;
-      }
-      else if (codigo == 4){
-        minerio = "Ouro";
-        resultado = Math.pow(qtrGramas, 5) * 1.5;
-      }
-      else{
-        System.out.print("Código errado, por gentileza contate um administrador!");
-        return;
-      }
-      System.out.print("O calculo para " + qtrGramas + " gramas de " + minerio + " é: " + resultado);
-    }
-    else{
-      System.out.print("Código errado, por gentileza contate um administrador!");
-    }
+    // // for (int i = 0; i < 10; i++){
+    // //   System.out.print(i + 1 +"º: ");
+    // //   valores += ler.nextDouble();
+    // // }
+    // // System.out.print("A soma dos valores é: " + valores);
 
 
 
-    // exercicio 01
-    Scanner ler = new Scanner(System.in);
-    Double A, B, C, resultado;
-    System.out.println("Escreva um algoritmo que leia três números inteiros e positivos (A, B, C) e calcule a seguinte expressão");
-    System.out.print("Digite o valor de A: ");
-    A = ler.nextDouble();
-    System.out.print("Digite o valor de B: ");
-    B = ler.nextDouble();
-    System.out.print("Digite o valor de C: ");
-    C = ler.nextDouble();
 
-    resultado = (Math.pow((A + B),2) + Math.pow((B + C),2)) / 2;
-    System.out.print("O resultado dá operação é " + resultado);
+    // // // exercicio 02
+    // // Scanner ler = new Scanner(System.in);
+    // // Double codigo, qtrGramas, resultado;
+    // // String minerio;
+    // // System.out.print("Digite o código do minério: ");
+    // // codigo = ler.nextDouble();
+    // // System.out.print("Digite a quantidade em gramas: ");
+    // // qtrGramas = ler.nextDouble();
+
+    // // if (codigo <= 4){
+    // //   if (codigo == 1){
+    // //     minerio = "Fosfato";
+    // //     resultado = (qtrGramas * 100) / 2;
+    // //   }
+    // //   else if (codigo == 2){
+    // //     minerio = "Nióbio";
+    // //     resultado = (qtrGramas + 100) / 3;
+    // //   }
+    // //   else if (codigo == 3){
+    // //     minerio = "Titânio";
+    // //     resultado = (qtrGramas * 1.5) / 5;
+    // //   }
+    // //   else if (codigo == 4){
+    // //     minerio = "Ouro";
+    // //     resultado = Math.pow(qtrGramas, 5) * 1.5;
+    // //   }
+    // //   else{
+    // //     System.out.print("Código errado, por gentileza contate um administrador!");
+    // //     return;
+    // //   }
+    // //   System.out.print("O calculo para " + qtrGramas + " gramas de " + minerio + " é: " + resultado);
+    // // }
+    // // else{
+    // //   System.out.print("Código errado, por gentileza contate um administrador!");
+    // // }
+
+
+
+    // // // exercicio 01
+    // // Scanner ler = new Scanner(System.in);
+    // // Double A, B, C, resultado;
+    // // System.out.println("Escreva um algoritmo que leia três números inteiros e positivos (A, B, C) e calcule a seguinte expressão");
+    // // System.out.print("Digite o valor de A: ");
+    // // A = ler.nextDouble();
+    // // System.out.print("Digite o valor de B: ");
+    // // B = ler.nextDouble();
+    // // System.out.print("Digite o valor de C: ");
+    // // C = ler.nextDouble();
+
+    // // resultado = (Math.pow((A + B),2) + Math.pow((B + C),2)) / 2;
+    // // System.out.print("O resultado dá operação é " + resultado);
 
 
 
